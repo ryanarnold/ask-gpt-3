736f6ecb4d7ffbc06bce5badc0a6e47fbc0833fe
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2023-01-02 15:17:12 +0800

    chore: setup tailwind

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>Hello</h1>
+        <h1 className="text-5xl">Hello</h1>
       </main>
     </>
   );
